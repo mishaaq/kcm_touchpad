2bb4e580f0525208770c74507eec579bbc390170
Michał Żarłok <mishaaq@gmail.com>
2009-10-17 11:51:59 +0200

    * disabled "touch sensitivity" option * added LICENSE and AUTHORS files * updated README file * added license notes in files

--- kcmtouchpad.cpp
+++ kcmtouchpad.cpp
@@ -1,3 +1,28 @@
+/*
+ * Copyright © 2009 Michał Żarłok
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the names of the authors
+ * not be used in advertising or publicity pertaining to distribution
+ * of the software without specific, written prior permission.  Authors
+ * makes no representations about the suitability of this software
+ * for any purpose.  It is provided "as is" without express or implied
+ * warranty.
+ *
+ * THE AUTHORS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+ * NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Authors: Michał Żarłok
+ */
+
 #include <QCheckBox>
 #include <QSlider>
 #include <QGroupBox>
@@ -104,9 +129,11 @@ void TouchpadConfig::enableProperties() {
         ui->TouchpadOnRB->setEnabled(TRUE);
         ui->TouchpadOffRB->setEnabled(TRUE);
     }
-    if (this->propertiesList.contains(SYNAPTICS_PROP_FINGER)) {
-        ui->TouchSensitivityGB->setEnabled(TRUE);
-    }
+/*    if (this->propertiesList.contains(SYNAPTICS_PROP_FINGER)) {
+        ui->SensitivityLowL->setEnabled(TRUE);
+        ui->SensitivityValueS->setEnabled(TRUE);
+        ui->SensitivityHighL->setEnabled(TRUE);
+    }*/
     if (this->propertiesList.contains(SYNAPTICS_PROP_SCROLL_EDGE)) {
         ui->ScrollVertEnableCB->setEnabled(TRUE);
         ui->ScrollHorizEnableCB->setEnabled(TRUE);
